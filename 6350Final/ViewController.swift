96e0a31573f7e7939f82399cc961005a3f62f2fd
Phoebe <huxiaodan829@gmail.com>
2021-12-13 20:12:29 -0800

    Initial Commit

---
+++ 6350Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6350Final
+//
+//  Created by Phoebe Hu on 12/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
